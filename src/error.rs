a0c08bdb815e84ffeb34efdb6a5bad273c6693f1
shonya3 <poeshonya3@gmail.com>
2023-05-22 09:21:51 +0300

    add MissingHeaders struct and return it, if file has no required headers

--- src/error.rs
+++ src/error.rs
@@ -6,6 +6,24 @@ use serde::Serialize;
 use crate::CARDS;
 
 #[derive(Debug)]
+pub struct MissingHeaders;
+
+impl Display for MissingHeaders {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "File should contain headers: name, amount")
+    }
+}
+
+impl serde::Serialize for MissingHeaders {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::ser::Serializer,
+    {
+        serializer.serialize_str(self.to_string().as_ref())
+    }
+}
+
+#[derive(Debug)]
 pub struct InvalidCardNameError(pub String);
 
 // impl std::error::Error for InvalidCardNameError {}
