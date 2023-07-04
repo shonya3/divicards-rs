47360406c65a5d54679e9caec1581e73ff66ae63
shonya3 <poeshonya3@gmail.com>
2023-07-04 19:28:49 +0300

    league mod

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 #![allow(unused)]
 pub mod error;
+pub mod league;
 
+use league::TradeLeague;
 use std::{collections::HashMap, fmt::Display, ops::Deref, path::Path, str::Lines, time::Instant};
 use urlencoding::encode as urlencode;
 
@@ -52,58 +54,6 @@ impl Default for Prices {
             .try_into()
             .unwrap();
         Prices(prices)
-    }
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub enum League {
-    Crucible,
-    Standard,
-    #[serde(alias = "Crucible-HC")]
-    HardcoreCrucible,
-    Hardcore,
-    #[serde(alias = "SSF Standard")]
-    SSFStandard,
-    #[serde(alias = "SSF Hardcore")]
-    SSFHardcore,
-    #[serde(alias = "SSF Crucible")]
-    SSFCrucible,
-    #[serde(alias = "HC SSF Crucible")]
-    SSFHCCrucible,
-}
-
-impl Display for League {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            League::Crucible => write!(f, "Crucible"),
-            League::Standard => write!(f, "Standard"),
-            League::HardcoreCrucible => write!(f, "Hardcore Crucible"),
-            League::Hardcore => write!(f, "Hardcore"),
-            League::SSFStandard => write!(f, "Solo Self-Found"),
-            League::SSFHardcore => write!(f, "Hardcore SSF"),
-            League::SSFCrucible => write!(f, "SSF Crucible"),
-            League::SSFHCCrucible => write!(f, "HC SSF Crucible"),
-        }
-    }
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub enum TradeLeague {
-    Crucible,
-    Standard,
-    #[serde(alias = "Crucible-HC")]
-    HardcoreCrucible,
-    Hardcore,
-}
-
-impl Display for TradeLeague {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            TradeLeague::Crucible => write!(f, "Crucible"),
-            TradeLeague::Standard => write!(f, "Standard"),
-            TradeLeague::HardcoreCrucible => write!(f, "Hardcore Crucible"),
-            TradeLeague::Hardcore => write!(f, "Hardcore"),
-        }
     }
 }
 
